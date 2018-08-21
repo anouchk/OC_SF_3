36db8acde483fdba3151a564981a0abad3b9a736
anouchk <analutzky@gmail.com>
2018-08-21 23:29:08 +0200

    générer des URL

--- Symfony/src/OC/PlatformBundle/Controller/AdvertController.php
+++ Symfony/src/OC/PlatformBundle/Controller/AdvertController.php
@@ -16,10 +16,19 @@ class AdvertController extends Controller
 	{
 		// On récupère la vue d'index
 		// NomDuBundle:NomDuContrôleur:NomDeLAction
-		$content = $this
-			->get('templating')
-			->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'winzou'));
-		return new Response ($content);
+		// $content = $this
+		// 	->get('templating')
+		// 	->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'winzou'));
+		// return new Response ($content);
+
+		// On veut avoir l'URL de l'annonce d'id 5.
+        $url = $this->get('router')->generate(
+            'oc_platform_view', // 1er argument : le nom de la route
+            array('id' => 5)    // 2e argument : les valeurs des paramètres
+        );
+        // $url vaut « /platform/advert/5 »
+
+        return new Response("L'URL de l'annonce d'id 5 est : ".$url);
 	}
 
 	public function byebyeAction()
