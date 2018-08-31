8e6fe83eb28f1d13bf27cc70a54e314cb0865268
saysa <sbo@conamed.de>
2018-08-31 16:08:11 +0200

    commit

--- Symfony/src/OC/PlatformBundle/Controller/AdvertController.php
+++ Symfony/src/OC/PlatformBundle/Controller/AdvertController.php
@@ -6,6 +6,7 @@ namespace OC\PlatformBundle\Controller;
 use Symfony\Component\HttpFoundation\Response;
 // Idem avec l'objet Request
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 // Pour accéder aux méthodes de gestion des templates, nous allons faire hériter notre contrôleur du contrôleur de base de Symfony, qui apporte quelques méthodes bien pratiques dont nous nous servirons tout au long de ce cours. 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -13,6 +14,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 // Le nom de notre contrôleur respecte le nom du fichier pour que l'autoload fonctionne :
 class AdvertController extends Controller
 {
+    /**
+     * @Route("/toto", name="toto")
+     */
+    public function annotationAction(Request $request)
+    {
+        echo $_GET['prenom'];
+        echo $request->query->get('prenom');
+
+        return new Response('test');
+    }
+
 	// On définit la méthodeindexAction(). N'oubliez pas de mettre le suffixe Action derrière le nom de la méthode :
 	public function indexAction()
 	{
@@ -43,14 +55,22 @@ class AdvertController extends Controller
 		return new Response ($content);
 	}
 
-	// La route fait appel à OCPlatformBundle:Advert:view,
-	// on doit donc définir la méthode viewAction.
-	// On donne à cette méthode l'argument $id, pour
-	// correspondre au paramètre {id} de la route
-	// Vous avez accès à la requête HTTP via $request
-	// On injecte la requête dans les arguments de la méthode
-	public function viewAction($id, Request $request)
 
+    /**
+     * La route fait appel à OCPlatformBundle:Advert:view,
+     * on doit donc définir la méthode viewAction.
+     * On donne à cette méthode l'argument $id, pour
+     * correspondre au paramètre {id} de la route
+     * Vous avez accès à la requête HTTP via $request
+     * On injecte la requête dans les arguments de la méthode
+     *
+     * @Route("/advert/{id}", name="oc_platform_view", requirements={"id"="\d+"})
+     * @param $id
+     * @param Request $request
+     *
+     * @return Response
+     */
+	public function viewAction($id, Request $request)
 	{
 	    // $id vaut 5 si l'on a appelé l'URL /platform/advert/5
 	    // Ici, on récupèrera depuis la base de données
