76bb0235f9bdf72782f36063e6b03b3944bac6ab
anouchk <analutzky@gmail.com>
2018-09-22 23:15:41 +0200

    testons les messages flash

--- Symfony/src/OC/PlatformBundle/Controller/AdvertController.php
+++ Symfony/src/OC/PlatformBundle/Controller/AdvertController.php
@@ -114,5 +114,22 @@ class AdvertController extends Controller
         );
     }
 
+    // Ajoutez cette méthode :
+
+	public function addAction(Request $request)
+
+	{
+    $session = $request->getSession();
+    // Bien sûr, cette méthode devra réellement ajouter l'annonce
+    // Mais faisons comme si c'était le cas
+
+    $session->getFlashBag()->add('info', 'Annonce bien enregistrée');
+    // Le « flashBag » est ce qui contient les messages flash dans la session
+    // Il peut bien sûr contenir plusieurs messages :
+    $session->getFlashBag()->add('info', 'Oui oui, elle est bien enregistrée !');
+    // Puis on redirige vers la page de visualisation de cette annonce
+    return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+    }
+
     
 }
