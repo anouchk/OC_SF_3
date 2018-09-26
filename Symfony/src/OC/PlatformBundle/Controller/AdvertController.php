cefb7168ff842a1a1a81885974236cfa59b41fcb
anouchk <analutzky@gmail.com>
2018-09-26 22:51:09 +0200

    une liste d'annonces en dur

--- Symfony/src/OC/PlatformBundle/Controller/AdvertController.php
+++ Symfony/src/OC/PlatformBundle/Controller/AdvertController.php
@@ -83,11 +83,57 @@ class AdvertController extends Controller
     	}
 
 	    // Ici, on récupérera la liste des annonces, puis on la passera au template
+	    // Notre liste d'annonce en dur
 
-	    // Mais pour l'instant, on ne fait qu'appeler le template
-	    return $this->render('OCPlatformBundle:Advert:index.html.twig', array(
-			'listAdverts' => array()
-		));
+    $listAdverts = array(
+
+      array(
+
+        'title'   => 'Recherche développpeur Symfony',
+
+        'id'      => 1,
+
+        'author'  => 'Alexandre',
+
+        'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+
+        'date'    => new \Datetime()),
+
+      array(
+
+        'title'   => 'Mission de webmaster',
+
+        'id'      => 2,
+
+        'author'  => 'Hugo',
+
+        'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+
+        'date'    => new \Datetime()),
+
+      array(
+
+        'title'   => 'Offre de stage webdesigner',
+
+        'id'      => 3,
+
+        'author'  => 'Mathieu',
+
+        'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+
+        'date'    => new \Datetime())
+
+    );
+
+
+    // Et modifiez le 2nd argument pour injecter notre liste
+
+    return $this->render('OCPlatformBundle:Advert:index.html.twig', array(
+
+      'listAdverts' => $listAdverts
+
+    ));
+
         
 	}
 
